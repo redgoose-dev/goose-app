efedd640c4e5e5c2e4784f81f88339e90d325aa8
redgoose-dev <scripter@me.com>
2018-10-23 23:20:46 +0900

    설정들 추가조정

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # goose-app
 
-...
+이 프로그램은 [goose-api](sdgsdg)
 
-## demo
+## demo url
 
 https://demo.redgoose.me
 
-## Build Setup
+
+## Install and start
 
 ``` bash
-# install dependencies
-$ yarn install
+# install
+$ ./script install
+
+# reinstall
+$ ./script reinstall
+
+# uninstall
+$ ./script uninstall
 
 # serve with hot reload at localhost:3000
 $ yarn run dev
@@ -18,7 +25,35 @@ $ yarn run dev
 # build for production and launch server
 $ yarn run build
 $ yarn start
-
-# generate static project
-$ yarn run generate
 ```
+
+
+## env
+
+빌드하기전에 직접 커스터마이즈를 할 수 있습니다.  
+먼저 인스톨을 하고 `/user` 경로에 `env.js`와 `env.scss` 파일을 변경해서 원하는 모습으로 고쳐서 사용할 수 있습니다.
+
+사용에 주의할점은 값을 수정하고 `yarn run build`로 다시 빌드하고 `yarn run start`를 해줘야합니다.
+
+### env.js
+기초적으로 필요한 부분들을 설정으로 빼두었습니다.
+
+### env.scss
+스타일의 일부분을 수정할 수 있습니다.
+
+
+## user data
+
+### page
+
+별도로 페이지를 추가하고 싶다면 `/pages/page` 경로에 `{filename}.vue` 이름의 파일로 만들고 `http://{URL}/page/{filename}` 형식으로 접속해서 사용할 수 있습니다.
+
+페이지에서 사용하는 이미지는 `/static`에 넣고, `/image.jpg` 형식으로 이미지를 불러올 수 있습니다.
+
+### favicon
+
+`/static/favicon.ico` 경로로 넣어서 사용할 수 있습니다.
+
+### user assets
+
+`/static` 경로에서 파일들을 추가해서 사용할 수 있습니다. `/static/images` 경로는 고정적으로 사용하는 요소들이므로 건드리지 마세요.
--- resource/env.js
+++ resource/env.js
@@ -1,8 +1,8 @@
 module.exports = {
 	app: {
 		name: 'goose-app',
-		description: 'gallery app from goose',
-		keywords: 'redgoose,redgoose-note,development,graphics,review',
+		description: 'website description',
+		keywords: 'development,graphics,review',
 		url: 'http://localhost:3000',
 		author: 'redgoose',
 		locale: 'ko_KR',
@@ -14,21 +14,19 @@ module.exports = {
 		},
 		app_srl: 8,
 		header: {
-			logo: '/user/ico-logo.png',
+			logo: '/ico-logo.png',
 			navigation: [
-				{ key: 'news', label: 'News', url: '/articles/demo-news' },
-				{ key: 'visual', label: 'Visual', url: '/articles/demo-visual' },
-				{ key: 'fashion', label: 'Fashion', url: '/articles/demo-fashion' },
-				{ key: 'review', label: 'Review', url: '/articles/demo-review' },
-				{ key: 'note', label: 'Note', url: '/articles/demo-note' },
-				{ key: 'redgoose', label: 'redgoose.me', url: 'https://redgoose.me', target: '_blank' },
+				{ key: 'menu1', label: 'Menu1', url: '/articles/menu1' },
+				{ key: 'menu2', label: 'Menu2', url: '/articles/menu2' },
+				{ key: 'menu3', label: 'Menu3', url: '/articles/menu3' },
+				{ key: 'redgoose', label: 'redgoose', url: 'https://redgoose.me', target: '_blank' },
 			],
 		},
 		intro: {
 			newest: {
 				size: 24,
 				pagination: true,
-				listStyle: 'card', // list style. ex)`card,thumbnail`
+				listStyle: 'thumbnail', // list style. ex)`card,thumbnail`
 				showMeta: {
 					date: true,
 					nestName: true,
@@ -58,12 +56,12 @@ module.exports = {
 			},
 		},
 		error: {
-			symbol: '/user/img-error.png',
+			symbol: '/img-error.png',
 			message: 'Service error',
 		},
 	},
 	api: {
-		url: 'https://goose.redgoose.me',
-		token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvZ29vc2UucmVkZ29vc2UubWUiLCJqdGkiOiI3YmNkZWZnaGlqa0Vtbm9SIiwiaWF0IjoxNTM0MzI1MjQwLCJkYXRhIjp7InR5cGUiOiJhbm9ueW1vdXMifX0.ZIBynqxpjjYOwrFVQ3854qhVIP4zICrOKQbAgLCsD0g',
+		url: '', // goose-api address
+		token: '', // goose-api public token
 	},
 };
